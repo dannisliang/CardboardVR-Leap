e4aa2c2b2187dba75c67dd05e9355ca732256678
Loren <loren.jk3@gmail.com>
2016-02-22 02:10:47 -0800

    Updated Trinus config, README, got hand tracking working properly, head tracking works properly as well

---
+++ Assets/Scripts/FaceTrackMovment.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class FaceTrackMovment : MonoBehaviour {
+
+    [SerializeField]
+    UDPReceive receiver = null;
+
+    Transform startPos = null;
+
+    [SerializeField]
+    float reductionPositionFactor = 1;
+
+    [SerializeField]
+    float reductionRotationFactor = 1;
+
+
+    void Start ()
+    {
+        startPos = transform;
+	}
+	
+	void Update ()
+    {
+        transform.position = new Vector3(receiver.xPos, 0, -receiver.zPos);
+        transform.rotation = Quaternion.Euler((receiver.yPos - 25), receiver.yaw, receiver.roll);
+    }
+}
---
+++ Assets/Scripts/UDPReceive.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+/*
+    Credit where credit is due (based off marcteys implementation):
+    https://github.com/marcteys/unityFaceTracking
+*/
+
+public class UDPReceive: MonoBehaviour {
+	
+	Thread receiveThread;
+	UdpClient client;
+    
+	public int port;
+    
+    // FaceAPI
+    public float xPos = 0;
+    public float yPos = 0;
+    public float zPos = 0;
+    public float pitch = 0;
+    public float yaw = 0;
+    public float roll = 0;
+	
+
+	public void Start() {				
+		receiveThread = new Thread(
+			new ThreadStart(ReceiveData));
+		receiveThread.IsBackground = true;
+		receiveThread.Start();
+	}
+	
+	// receive thread 
+	private void ReceiveData() {
+		
+		client = new UdpClient(port);
+		while (receiveThread != null) {
+			try {
+				IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+				byte[] data = client.Receive(ref anyIP);
+
+                for (int i = 0; i < 6; i++)	{
+   					double datum = BitConverter.ToDouble(data, i * 8);
+					var j = i+1;
+					switch (j)
+					{
+						case 1: {
+							xPos = (float)datum;
+						    break;
+						} case 2: {
+							pitch = (float)datum;
+						    break;
+						} case 3: {
+							zPos = (float)datum;
+							break;
+						} case 4: {
+							yaw = (float)datum;
+						    break;
+						} case 5: {
+                            yPos = (float)datum;
+                            break;
+						} case 6: {
+						    roll = (float)datum;
+							break;
+						}
+					}
+				}
+			} catch (Exception err) {
+				print(err.ToString());
+			}
+		}
+	}
+    
+	void OnDisable() {
+		if (receiveThread != null)
+			receiveThread.Abort();
+		client.Close();
+	}
+
+	void OnApplicationQuit() {
+		if (receiveThread != null)
+			receiveThread.Abort();
+		client.Close();
+	}
+}
